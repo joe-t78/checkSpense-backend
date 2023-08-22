3d571f5f78ac1eb8296e2fec4831e83b04eeebc3
joe-t78 <joetonyaashish@gmail.com>
2023-08-22 17:18:50 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use('/api/transactions/' , transactionsRoute)
 
 const port =process.env.PORT || 4000
 
-if(process.env.NODE_ENV === 'production')
-{
-     app.use('/' , express.static('client/build'))
+// if(process.env.NODE_ENV === 'production')
+// {
+//      app.use('/' , express.static('client/build'))
 
-     app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-     })
-}
+//      app.get('*' , (req, res)=>{
+//          res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+//      })
+// }
 
 
 app.listen(port,()=>console.log('Node JS server started!'));
